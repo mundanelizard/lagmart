b66dbcd0c1c2c7a1b92b14906b021792acfcef12
Mundane Lizard <mundanelizard@gmail.com>
2021-03-20 15:06:10 +0100

    created application boiler plate

--- routes/users.ts
+++ routes/users.ts
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(_req: any, res: { send: (arg0: string) => void; }, _next: any) {
   res.send('respond with a resource');
 });
 
